f82e5cf5cf4a3ed8fd38af087569bf46c9220aac
luisaGonzales <luisagc711@gmail.com>
2017-07-18 14:02:24 -0500

    Modificando espacios

--- Ejercicio Extra/js/javascript.js
+++ Ejercicio Extra/js/javascript.js
@@ -1,9 +1,6 @@
-
-
 function convertirCelsius(gradosC){
   var gradosC = document.getElementById('celsius').value;
   var resultado = document.getElementById("resultado");
   var farenheit = parseFloat(gradosC * 9/5 + 32);
   resultado.innerHTML = farenheit;
-
 }

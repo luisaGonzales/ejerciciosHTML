9801806d3f2140d25cd78dde94c1cb94e75b2240
luisaGonzales <luisagc711@gmail.com>
2017-07-18 13:58:47 -0500

    Subiendo archivos iniciales

---
+++ Ejercicio Extra/js/javascript.js
@@ -0,0 +1,9 @@
+
+
+function convertirCelsius(gradosC){
+  var gradosC = document.getElementById('celsius').value;
+  var resultado = document.getElementById("resultado");
+  var farenheit = parseFloat(gradosC * 9/5 + 32);
+  resultado.innerHTML = farenheit;
+
+}
